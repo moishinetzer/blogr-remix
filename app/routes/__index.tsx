41a3e5e72e2ee10582539bca3c27844f0a648af0
Moishi Netzer <moishinetzer@gmail.com>
2022-02-15 16:16:17 +0000

    Click sidebar to view note

--- app/routes/__index.tsx
+++ app/routes/__index.tsx
@@ -1,4 +1,4 @@
-import { LoaderFunction, Outlet, useLoaderData } from "remix";
+import { Link, LoaderFunction, Outlet, useLoaderData } from "remix";
 import { db } from "~/utils/db.server";
 
 export type Post = {
@@ -19,15 +19,16 @@ export const loader: LoaderFunction = async () => {
 
 export default function Index() {
   const posts = useLoaderData<Post[]>();
-  console.log(posts);
   return (
     <>
-      <div className="bg-blue-200">Header</div>
+      <Link to={"/"} className="bg-blue-200 w-screen">
+        Home
+      </Link>
       <div className="flex h-screen w-full">
         <div className="bg-blue-300 w-2/5 flex-auto">
           {posts.map((post) => (
             <div key={post.id}>
-              <p>{post.title}</p>
+              <Link to={`/post/${post.title}`}>{post.title}</Link>
             </div>
           ))}
         </div>
--- app/routes/__index/post/$post.tsx
+++ app/routes/__index/post/$post.tsx
@@ -1,3 +1,25 @@
+import { LoaderFunction, useLoaderData } from "remix";
+import { db } from "~/utils/db.server";
+import type { Post } from "~/routes/__index";
+
+export const loader: LoaderFunction = async ({ params }) => {
+  // Get post from database
+  console.log(params);
+  const post = await db.post.findFirst({
+    where: {
+      title: params.post,
+    },
+  });
+  return post;
+};
+
 export default function Post() {
-  return <h1>Post!</h1>;
+  const post = useLoaderData<Post>();
+  console.log(post);
+  return (
+    <>
+      <h1>{post.title}</h1>
+      <p>{post.content}</p>
+    </>
+  );
 }
