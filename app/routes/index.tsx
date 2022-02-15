9c502afb99deb428f8d53044f7ded72c1919ec96
Moishi Netzer <moishinetzer@gmail.com>
2022-02-15 14:55:54 +0000

    Load default data to homepage

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,8 +1,36 @@
+import { LoaderFunction, Outlet, useLoaderData } from "remix";
+
+export type Post = {
+  title: string;
+  content: string;
+};
+
+export const loader: LoaderFunction = async () => {
+  return [
+    { title: "My new post", content: "Hello world" },
+    { title: "My second post", content: "Second post content" },
+    { title: "My third post", content: "Third post content" },
+  ];
+};
+
 export default function Index() {
+  const posts = useLoaderData<Post[]>();
+  console.log(posts);
   return (
-    <div className="w-full mx-auto">
-      <h1>Welcome to Remix</h1>
-      <h1>Welcome to Remix</h1>
-    </div>
+    <>
+      <div className="bg-blue-200">Header</div>
+      <div className="flex h-screen w-full">
+        <div className="bg-blue-300 w-2/5 flex-auto">
+          {posts.map((post) => (
+            <div key={post.title}>
+              <p>{post.title}</p>
+            </div>
+          ))}
+        </div>
+        <div className="bg-blue-400 w-full flex-auto">
+          <Outlet />
+        </div>
+      </div>
+    </>
   );
 }
