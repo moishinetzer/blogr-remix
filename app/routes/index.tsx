fbf5e847c5de746732253c5b30ee2ae6b1366529
Moishi Netzer <moishinetzer@gmail.com>
2022-02-15 15:16:34 +0000

    Connect prisma to db and get post titles from it

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,16 +1,21 @@
 import { LoaderFunction, Outlet, useLoaderData } from "remix";
+import { getAllPosts } from "~/post";
+import { db } from "~/utils/db.server";
 
 export type Post = {
+  id: string;
   title: string;
   content: string;
+  published: boolean;
 };
 
 export const loader: LoaderFunction = async () => {
-  return [
-    { title: "My new post", content: "Hello world" },
-    { title: "My second post", content: "Second post content" },
-    { title: "My third post", content: "Third post content" },
-  ];
+  return await db.post.findMany({
+    select: {
+      id: true,
+      title: true,
+    },
+  });
 };
 
 export default function Index() {
@@ -22,7 +27,7 @@ export default function Index() {
       <div className="flex h-screen w-full">
         <div className="bg-blue-300 w-2/5 flex-auto">
           {posts.map((post) => (
-            <div key={post.title}>
+            <div key={post.id}>
               <p>{post.title}</p>
             </div>
           ))}
